0edcc68efb2f2f2b442313cae58f5810256d8360
aryngpt11 <aryan11gs@gmail.com>
2024-07-18 15:28:51 +0530

    demo

---
+++ enumeratee.py
@@ -0,0 +1,12 @@
+a=["A","B","C","D"]
+""" e=enumerate(a,start=4)
+print(list(e)) """
+
+
+#for loop
+""" for pos,ele in enumerate(a,9):
+    print(f'{pos}:{ele}') """
+
+#dictionary 
+
+print(dict(list(enumerate(a,1))))
